595b581d51e6fd1bb974d2489e26f0875b070815
Vetal-V <vvrublevsyi@gmail.com>
2020-05-25 22:07:50 +0300

    Update backend app callboard and gallery

--- backend/callboard/views.py
+++ backend/callboard/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from django.views.generic import ListView
+from .models import Advert
+# from .serializers import AdvertListSer, AdvertDetailSer, AdvertCreateSer
 
-# Create your views here.
+
+class AdvertList(ListView):
+    """Всі оголошення"""
+    # permission_classes = [permissions.AllowAny]
+    model = Advert
+    queryset = Advert.objects.all()
+    template_name = "callboard/advert-list.html"
+    # serializer_class = AdvertListSer
--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,3 +1,61 @@
+import os
+from PIL import Image
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.utils import timezone
+
+
+def get_path_upload_image(file):
+    """
+    make path of uploaded file shorter and return it
+    in following format: (media)/profile_pics/user_1/myphoto_2018-12-2.png
+    """
+    time = timezone.now().strftime("%Y-%m-%d")
+    end_extention = file.split('.')[1]
+    head = file.split('.')[0]
+    if len(head) > 10:
+        head = head[:10]
+    file_name = head + '_' + timezone.now().strftime("%h-%m-%s") + '.' + end_extention
+    return os.path.join('photos', '{}', '{}').format(time, file_name)
+
+
+class Photo(models.Model):
+    """Фото"""
+    name = models.CharField("Ім'я", max_length=50)
+    image = models.ImageField("Фото", upload_to="gallery/")
+    created = models.DateTimeField("Дата створення", auto_now_add=True)
+    slug = models.SlugField("url", max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.image.name = get_path_upload_image(self.image.name)
+        super().save(*args, **kwargs)
+
+        # if self.image:
+        #     img = Image.open(self.image.path)
+        #     if img.height > 200 or img.width > 200:
+        #         output_size = (200, 200)
+        #         img.thumbnail(output_size)
+        #         img.save(self.image.path)
+
+    class Meta:
+        verbose_name = "Зображення"
+        verbose_name_plural = "Зображення"
+
+
+class Gallery(models.Model):
+    """Галерея"""
+    name = models.CharField("Ім'я", max_length=50)
+    photos = models.ManyToManyField(Photo, verbose_name="Фотографії")
+    created = models.DateTimeField("Дата створення", auto_now_add=True)
+    slug = models.SlugField("url", max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Галерея"
+        verbose_name_plural = "Галерея"
